341bda8ca1d0de876b70db482afe794677029165
unknown <tojorodialson@gmail.com>
2019-11-18 21:11:21 +0300

    add meta propeller on top

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="propeller" content="465f0d77882d9aca85b7afcaa09fb560">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="propeller" content="465f0d77882d9aca85b7afcaa09fb560">
     <meta name="description" content="I'm Tojo RODIALSON, backend developer since 2014. I choose this job because I have a passion for codes. I've been working on different projects in recent years to always improve how to code and meet different people.">
     <meta name="keywords" content="Tojo RODIALSON, backend developer, back-end developer, web developer, website builder, developer, freelancer developer, laravel developer, php developer, java developer, c# developer">
     <link rel="icon" href="{{asset('assets/img/favicon.png')}}" type="image/png">

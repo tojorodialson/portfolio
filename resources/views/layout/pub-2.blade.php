b9628b9dc20c7d9ae43e6777244092a7c5ce1ed4
unknown <tojorodialson@gmail.com>
2019-11-18 21:48:36 +0300

    fix pub

--- resources/views/layout/pub-2.blade.php
+++ resources/views/layout/pub-2.blade.php
@@ -1,2 +1,1 @@
 <script async="async" data-cfasync="false" src="//pl15225786.passeura.com/80e1381ba0062b910174521d914b5df5/invoke.js"></script>
-<div id="container-80e1381ba0062b910174521d914b5df5"></div>
